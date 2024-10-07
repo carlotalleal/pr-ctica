345fb1c703c198d639ac906ef45236b44217b9cd
carlota <carlota.llealalonso@gmail.com>
2024-10-07 09:46:56 +0200

    practica1 nano

--- practica1.sh
+++ practica1.sh
@@ -1,4 +1,9 @@
+
 #!/bin/bash
 	cut -d',' -f 1-11,13-15 supervivents.csv > temporal.csv
+	awk -F ',' '{ if ($14 == "False") print $0 }' temporal.csv >> temporal2.csv
+	a=$(wc -l < temporal.csv) 
+	b=$(wc -l < temporal2.csv)
+	numerror=$((a-b))
 fi
 
