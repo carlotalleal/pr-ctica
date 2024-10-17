02d8b6542df77aef475a835a08856990ed8823e3
carlota <carlota.llealalonso@gmail.com>
2024-10-17 21:08:03 +0200

    Sortida

--- practica1.sh
+++ practica1.sh
@@ -1,28 +1,38 @@
 
-#!/bin/bash
+#!/binbash
+#Punt 1
 	cut -d',' -f 1-11,13-15 supervivents.csv > temporal.csv
+#Punt 2
 	awk -F ',' '{ if ($14 == "False") print $0 }' temporal.csv >> temporal2.csv
-	a=$(wc -l < temporal.csv) 
+	a=$(wc -l < temporal.csv)
 	b=$(wc -l < temporal2.csv)
 	numerror=$((a-b))
+
+#Punt 3
 	awk -F',' '{ if ($8 < 1000000) {print $0 ",Bo"} else if ($8 <= 10000000) {print $0 ",Excel·lent"} else{print $0 ",Estrella"}}' temporal2.csv >> temporal3.csv
-	awk -F',' '{print $0 ","$9*100/$8}' temporal3.csv >> temporal4.csv
-	awk -F',' '{print $0 ","$10*100/$8}' temporal4.csv >> temporal5.csv
+
+#Punt 4
 	cont=0
-	while $cont<$b; do
-		read x;
-		likes=echo$x | cut -d',' -f9;
-		dislikes=echo$x | cut -d',' -f10;
-		views=echo$x | cut -d',' -f8;
-		echo ","$likes*100/views temporal3.csv >> temporal4.csv;
-		echo ","$likes*100/views temporal4.csv >> temporal5.csv;
-	cont=$cont+1
+	while  [ $cont -lt $b ]; do
+		read x
+    		likes=$(echo $x | cut -d',' -f9)
+    		dislikes=$(echo $x | cut -d',' -f10)
+    		views=$(echo $x | cut -d',' -f8)
+		y=$((likes*100/views))
+		z=$((dislikes*100/views))
+		echo "$x,$y,$z" >> sortida.csv
+	cont=$((cont+1))
 	done < temporal3.csv
-	id=$($1)
-	if (grep -w "^$id" temporal5.csv); then
-		x=True
-	else
-		echo "No s'ha trbat cap coincidència."
+
+#Punt 5
+	if ["$#" -eq 1]; then
+		id=$1
+		sortida=$(grep -w "^$id" sortida.csv)
+		if (grep -n "^$sortida"); then
+			echo "$id" | cut -d ',' -f3,6,8,9,10,15,16,17
+		else
+			echo "No s'ha trobat cap coincidència."
+		fi
 	fi
 fi
 
